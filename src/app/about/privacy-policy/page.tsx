b4c37f22c67f605d246b3e0e4bcbed52bb9cbee3
Raf Ari <baltarias@gmail.com>
2024-10-11 14:46:37 -0700

    updates to app 4

--- src/app/about/privacy-policy/page.tsx
+++ src/app/about/privacy-policy/page.tsx
@@ -8,71 +8,71 @@ export default function page( ) {
         <p style={{ fontSize: '24px', fontWeight: 'bold' }}>Privacy Policy</p>
         <p style={{ fontSize: '14px' }}>Last updated: 11/10/2024</p>
         <p>
-          At Media SSN, we are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our app and services. Please read this policy carefully to understand our views and practices regarding your personal data and how we will treat it.
+          {"At Media SSN, we are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our app and services. Please read this policy carefully to understand our views and practices regarding your personal data and how we will treat it."}
         </p>
         <p>
           1. Information We Collect
-          We collect several types of information from and about users of the Media SSN app, including:
+          {"We collect several types of information from and about users of the Media SSN app, including:"}
 
-          Personal Information: Information that can be used to identify you, such as your name, email address, and any other information you provide when registering for an account.
-          Media Files: Photos and videos you capture and upload through the app. These media files may contain metadata (e.g., timestamps, geolocation data) used for verification purposes.
-          Device Information: We may collect information about your mobile device, including the hardware model, operating system version, unique device identifiers, and mobile network information.
-          Usage Data: Details of your use of the app, including traffic data, logs, and other communication data related to your activities in the app.
-          We only collect data that is necessary for the functionality and security of the app.
+          {"Personal Information: Information that can be used to identify you, such as your name, email address, and any other information you provide when registering for an account."}
+          {"Media Files: Photos and videos you capture and upload through the app. These media files may contain metadata (e.g., timestamps, geolocation data) used for verification purposes."}
+          {"Device Information: We may collect information about your mobile device, including the hardware model, operating system version, unique device identifiers, and mobile network information."}
+          {"Usage Data: Details of your use of the app, including traffic data, logs, and other communication data related to your activities in the app."}
+          {"We only collect data that is necessary for the functionality and security of the app."}
         </p>
         <p>
           2. How We Use Your Information
-          We use the information we collect in the following ways:
+          {"We use the information we collect in the following ways:"}
 
-          To Verify Media: Your photos and videos are analyzed and stored to verify authenticity, assign a unique ID, and allow third parties to confirm the content’s integrity.
-          To Improve Our Services: We analyze usage data to improve app functionality and user experience.
-          To Communicate with You: We may use your contact information to send updates, respond to support requests, or notify you of important changes.
-          For Security: We use the collected information to protect against unauthorized access, fraud, and other security issues.
+          {"To Verify Media: Your photos and videos are analyzed and stored to verify authenticity, assign a unique ID, and allow third parties to confirm the content’s integrity."}
+          {"To Improve Our Services: We analyze usage data to improve app functionality and user experience."}
+          {"To Communicate with You: We may use your contact information to send updates, respond to support requests, or notify you of important changes."}
+          {"For Security: We use the collected information to protect against unauthorized access, fraud, and other security issues."}
         </p>
         <p>
           3. How We Share Your Information
-          We will not share your personal information with third parties except in the following cases:
+          {"We will not share your personal information with third parties except in the following cases:"}
 
-          Verification Requests: When someone requests to verify the authenticity of media files, we will only share the media's verification status, ID, and related metadata. No personal information will be disclosed unless you explicitly authorize it.
-          Service Providers: We may share your information with third-party service providers who perform services on our behalf, such as cloud storage and data analysis. These providers are bound by confidentiality agreements.
-          Legal Obligations: We may disclose your information if required to do so by law or in response to valid legal requests, such as a subpoena, court order, or government investigation.
-          We do not sell or rent your personal information to third parties.
+          {"Verification Requests: When someone requests to verify the authenticity of media files, we will only share the media's verification status, ID, and related metadata. No personal information will be disclosed unless you explicitly authorize it."}
+          {"Service Providers: We may share your information with third-party service providers who perform services on our behalf, such as cloud storage and data analysis. These providers are bound by confidentiality agreements."}
+          {"Legal Obligations: We may disclose your information if required to do so by law or in response to valid legal requests, such as a subpoena, court order, or government investigation."}
+          {"We do not sell or rent your personal information to third parties."}
         </p>
         <p>
           4. Data Security
-          We are committed to protecting the security of your data. We use industry-standard encryption and security measures to safeguard your personal information and media files. However, no method of transmission over the Internet or electronic storage is 100% secure. While we strive to protect your data, we cannot guarantee its absolute security.
+          {"We are committed to protecting the security of your data. We use industry-standard encryption and security measures to safeguard your personal information and media files. However, no method of transmission over the Internet or electronic storage is 100% secure. While we strive to protect your data, we cannot guarantee its absolute security."}
         </p>
         <p>
           5. Data Retention
-          We will retain your personal information and media files for as long as necessary to provide our services to you or as required by applicable laws. Once your data is no longer needed, we will securely delete or anonymize it.
+          {"We will retain your personal information and media files for as long as necessary to provide our services to you or as required by applicable laws. Once your data is no longer needed, we will securely delete or anonymize it."}
         </p>
         <p>
           6. Your Rights and Choices
-          You have the right to:
+          {"You have the right to:"}
 
-          Access and Update: Request access to your personal information and make corrections if necessary.
-          Delete: Request the deletion of your personal information and media files from our servers, subject to any legal obligations we may have to retain certain data.
-          Opt-Out: You can opt out of marketing communications at any time by following the unsubscribe instructions provided in those communications.
-          To exercise these rights, please contact us at support@mediassn.com.
+          {"Access and Update: Request access to your personal information and make corrections if necessary."}
+          {"Delete: Request the deletion of your personal information and media files from our servers, subject to any legal obligations we may have to retain certain data."}
+          {"Opt-Out: You can opt out of marketing communications at any time by following the unsubscribe instructions provided in those communications."}
+          {"To exercise these rights, please contact us at support@mediassn.com."}
         </p>
         <p>
           7. Privacy for Children
-          Media SSN is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under 13. If we learn that we have collected such information, we will take steps to delete it.
+          {"Media SSN is not intended for use by children under the age of 13. We do not knowingly collect personal information from children under 13. If we learn that we have collected such information, we will take steps to delete it."}
         </p>  
         <p>
           8. International Data Transfers
-          If you are accessing our services from outside of the United States, please note that your information may be transferred to, and processed in, the United States where data protection laws may differ. By using our app, you consent to the transfer of your information to our servers located in the United States.
+          {"If you are accessing our services from outside of the United States, please note that your information may be transferred to, and processed in, the United States where data protection laws may differ. By using our app, you consent to the transfer of your information to our servers located in the United States."}
         </p>
         <p>
           9. Changes to This Privacy Policy
-          We may update this Privacy Policy from time to time. We will notify you of any significant changes by updating the "Effective Date" at the top of this policy. Your continued use of the app after the changes take effect will constitute your acceptance of the updated policy.
+          {"We may update this Privacy Policy from time to time. We will notify you of any significant changes by updating the Effective Date at the top of this policy. Your continued use of the app after the changes take effect will constitute your acceptance of the updated policy."}
         </p>
         <p>
           10. Contact Us
-          If you have any questions or concerns about this Privacy Policy or your personal data, please contact us at:
+          {"If you have any questions or concerns about this Privacy Policy or your personal data, please contact us at:"}
 
-          Email: support@mediassn.com
-          Mailing Address: 1234 Main Street, Snohomish, WA 98290
+          {"Email: support@mediassn.com"}
+          {"Mailing Address: 1234 Main Street, Snohomish, WA 98290"}
         </p>
         
       </div>
